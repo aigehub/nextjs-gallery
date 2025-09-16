697ec78bca4da60a5f8c22cbc1ecf7df32497f52
jaysen.lin <jaysen.lin@foxmail.com>
2025-09-17 00:37:19 +0800

    格式化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import TopButton from "./components/TopButton";
 import SearchComponent from "./components/Search";
 import { PAGE_SIZE, queryData } from "./libs/data";
 
-
 export default async function Home(props: {
   searchParams: Promise<{
     page?: string;
@@ -17,19 +16,16 @@ export default async function Home(props: {
   }>;
 }) {
   const params = await props.searchParams; // ✅ 这里要 await
-  const {
-      max_price,
-      province,
-      bust}=params
+  const { max_price, province, bust } = params;
   const page = parseInt(params.page || "1");
   console.log("Home page params:", params);
-      const { total, page_data } = await queryData({
-      max_price,
-      province,
-      bust,
-      offset: page || 1,
-      limit: PAGE_SIZE,
-    });
+  const { total, page_data } = await queryData({
+    max_price,
+    province,
+    bust,
+    offset: page || 1,
+    limit: PAGE_SIZE,
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-14 sm:p-5 p-5">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
