4749e50e52e896ec1f9372ef9d7abbf77817ede6
jaysen.lin <jaysen.lin@foxmail.com>
2025-09-21 01:50:17 +0800

    增加预览图片框架，但是还有一些问题，预览有些加载不出来。

--- src/app/components/ZZVideo.tsx
+++ src/app/components/ZZVideo.tsx
@@ -4,41 +4,51 @@ import { useEffect, useState } from "react";
 
 interface SmartVideoProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
   src: string;
+  plat: number;
 }
 
 /**
  * 在前端 fetch 视频得到 Blob URL，再用 <video src="blob:...">
  */
-export default function SmartVideo({ src, poster, className, ...props }: SmartVideoProps) {
+export default function SmartVideo({ src, plat, poster, className, ...props }: SmartVideoProps) {
   const [blobUrl, setBlobUrl] = useState<string | null>(null);
 
   useEffect(() => {
     if (!src) return;
+    if (plat == 2) {
+      let currentUrl: string | null = null;
+      (async () => {
+        try {
+          const res = await fetch(src);
+          if (!res || !res.ok) throw new Error("HTTP " + res.status);
+          const blob = await res.blob();
+          currentUrl = URL.createObjectURL(blob);
+          setBlobUrl(currentUrl);
+        } catch (e) {
+          console.log(e);
+          if (e instanceof Error) {
+            console.log(e.stack);
+          }
+        }
+      })().catch(console.error);
 
-    let currentUrl: string | null = null;
+      // 组件卸载时释放 blob URL
+      return () => {
+        if (currentUrl) URL.revokeObjectURL(currentUrl);
+      };
+    } else {
+      setBlobUrl(src);
+    }
+  }, [src, plat]);
 
-    (async () => {
-      const res = await fetch(src);
-      if (!res.ok) throw new Error("HTTP " + res.status);
-      const blob = await res.blob();
-      currentUrl = URL.createObjectURL(blob);
-      setBlobUrl(currentUrl);
-    })().catch(console.error);
-
-    // 组件卸载时释放 blob URL
-    return () => {
-      if (currentUrl) URL.revokeObjectURL(currentUrl);
-    };
-  }, [src]);
-
-  if (!blobUrl) return <div className={className}>视频加载中...</div>;
+  if (!blobUrl) return <div className={`bg-black rounded-lg transition-opacity duration-500 w-full h-full ${className}`}>视频加载中...</div>;
   return (
     <video
       {...props}
       controls
       poster={poster}
       src={blobUrl} // blob:xxxx
-      className={`bg-black rounded-lg mb-4 transition-opacity duration-500 w-full h-full ${className}`}
+      className={`bg-black rounded-lg transition-opacity duration-500 w-full h-full ${className}`}
     />
   );
 }
